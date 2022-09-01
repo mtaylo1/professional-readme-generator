6d37f6be94dc807a7828e114684e9d42ed355e05
M Taylor <107767318+mtaylo1@users.noreply.github.com>
2022-09-01 19:09:48 -0400

    re-editing readme.md file

--- README.md
+++ README.md
@@ -1,31 +1,31 @@
 # 
     Professional ReadMe Generator
-    ## License:
-    ## License: MIT  ![GitHub license](https://img.shields.io/badge/License-MIT-yellow.svg)
-    ### https://choosealicense.com/licenses/mit/
-    [![license] MIT]
+   ## License:
+   ## License: MIT  ![GitHub license](https://img.shields.io/badge/License-MIT-yellow.svg)
+   ### https://choosealicense.com/licenses/mit/
+   [![license] MIT]
     
-    ## Table of contents:
-    -[Description](#description)
-    -[Installation](#installation)
-    -[Usage](#usage)
-    -[Contribution](#contribution)
-    -[Testing](#testing)
-    -[Additional Info](#addtional-info)
+   ## Table of contents:
+   -[Description](#description)
+   -[Installation](#installation)
+   -[Usage](#usage)
+   -[Contribution](#contribution)
+   -[Testing](#testing)
+   -[Additional Info](#addtional-info)
 
-    ## Description:
-    This is a professional ReadMe Generator. This will prompt the user with some questions and create a ReadMe based on it. 
-    ## Installation:
-    This can be installed by downloading into a zip and extracting the files on your computer. 
-    ## Usage:
-    This quickly creates a professional ReadMe.md for whatever project you are working on
-    ## License:
-    MIT
-    ## Contribution:
-    Just me :)
-    ## Testing:
-    run index.js in your terminal to test this app
-    ## Contact Information: 
-    -Github: mtaylo1 (https://github.com/mtaylo1)
-    -Email: fakeemail@gmail.com
-    
+   ## Description:
+   This is a professional ReadMe Generator. This will prompt the user with some questions and create a ReadMe based on it. 
+   ## Installation:
+   This can be installed by downloading into a zip and extracting the files on your computer. 
+   ## Usage:
+   This quickly creates a professional ReadMe.md for whatever project you are working on
+   ## License:
+   MIT
+   ## Contribution:
+   Just me :)
+   ## Testing:
+   This can be installed by downloading into a zip and extracting the files on your computer. 
+   ## Contact Information: 
+   -Github: mtaylo1 (https://github.com/mtaylo1)
+   -Email: fakeemail@gmail.com
+   
--- src/runFile.js
+++ src/runFile.js
@@ -2,78 +2,78 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
     let badge = '';
-    if(license === 'MIT') {
+    if (license === 'MIT') {
         badge = '![GitHub license](https://img.shields.io/badge/License-MIT-yellow.svg)'
     } else if (license === 'Apache 2.0') {
         badge = '![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)'
     } else if (license === 'GPL v3.0') {
         badge = '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)'
     } else {
-      badge = ""
+        badge = ""
     }
     return badge;
-  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
     let licenseLink = '';
-      if(license === 'MIT') {
+    if (license === 'MIT') {
         licenseLink = 'https://choosealicense.com/licenses/mit/'
-      } else if (license === 'Apache 2.0') {
+    } else if (license === 'Apache 2.0') {
         licenseLink = 'http://www.apache.org/licenses/LICENSE-2.0'
-      } else if (license === 'GPL v3.0') {
+    } else if (license === 'GPL v3.0') {
         licenseLink = 'https://www.gnu.org/licenses'
-      } else {
+    } else {
         licenseLink = ""
-      }
-      return licenseLink;
-  }
+    }
+    return licenseLink;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  let licenseSection = ''
-  if(license === 'None') {
-    licenseSection = ''
-  } else {
-    licenseSection =
-    `License: ${license} `
-  }
-  return licenseSection;
+    let licenseSection = ''
+    if (license === 'None') {
+        licenseSection = ''
+    } else {
+        licenseSection =
+            `License: ${license} `
+    }
+    return licenseSection;
 }
 function runFile(data) {
-    
+
     return `# 
     ${data.title}
-    ## License:
-    ## ${renderLicenseSection(data.license)} ${renderLicenseBadge(data.license)}
-    ### ${renderLicenseLink(data.license)}
-    [![license] ${data.license}]
+   ## License:
+   ## ${renderLicenseSection(data.license)} ${renderLicenseBadge(data.license)}
+   ### ${renderLicenseLink(data.license)}
+   [![license] ${data.license}]
     
-    ## Table of contents:
-    -[Description](#description)
-    -[Installation](#installation)
-    -[Usage](#usage)
-    -[Contribution](#contribution)
-    -[Testing](#testing)
-    -[Additional Info](#addtional-info)
+   ## Table of contents:
+   -[Description](#description)
+   -[Installation](#installation)
+   -[Usage](#usage)
+   -[Contribution](#contribution)
+   -[Testing](#testing)
+   -[Additional Info](#addtional-info)
 
-    ## Description:
-    ${data.description}
-    ## Installation:
-    ${data.installation}
-    ## Usage:
-    ${data.usage}
-    ## License:
-    ${data.license}
-    ## Contribution:
-    ${data.contribute}
-    ## Testing:
-    ${data.test}
-    ## Contact Information: 
-    -Github: ${data.github} (https://github.com/${data.github})
-    -Email: ${data.email}
-    `};
+   ## Description:
+   ${data.description}
+   ## Installation:
+   ${data.installation}
+   ## Usage:
+   ${data.usage}
+   ## License:
+   ${data.license}
+   ## Contribution:
+   ${data.contribute}
+   ## Testing:
+   ${data.test}
+   ## Contact Information: 
+   -Github: ${data.github} (https://github.com/${data.github})
+   -Email: ${data.email}
+   `};
 
-    module.exports = runFile;
+module.exports = runFile;
